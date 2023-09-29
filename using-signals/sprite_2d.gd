e0803d3905ae004c226e4a4080c36d0a08c9fa4c
harmonia2side <breno_odnan@hotmail.com>
2023-09-29 02:07:21 -0300

    finaliza tutorial de sinais

--- using-signals/sprite_2d.gd
+++ using-signals/sprite_2d.gd
@@ -7,11 +7,21 @@ var speed = 400
 var angular_speed = PI
 
 func _process(delta):
+	# observe que #rotation é uma propriedade herdada
 	rotation += angular_speed * delta
+	# Para se mover em círculos
 	var velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
 
 
+
 func _on_button_pressed():
-	# Replace with function body.
 	set_process(not is_processing())
+	
+func _ready():
+	var timer = get_node("Timer")
+	timer.timeout.connect(_on_timer_timeout)
+
+func _on_timer_timeout():
+	visible = not visible
+	
