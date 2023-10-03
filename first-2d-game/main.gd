671020da63c3f4dcccc49555d7ac1514606b9d63
harmonia2side <breno_odnan@hotmail.com>
2023-10-03 01:14:23 -0300

    Retorna o código a estado testável
    Rodar a cena do player ou a mais agora vai mostrar o jogador

--- first-2d-game/main.gd
+++ first-2d-game/main.gd
@@ -13,8 +13,8 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#func _process(delta):
+#	pass
 
 func game_over():
 	$ScoreTimer.stop()
--- first-2d-game/player.gd
+++ first-2d-game/player.gd
@@ -10,20 +10,20 @@ var screen_size			# Tamanho da janela de jogo
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-	hide()
-	var player = get_node(".")
-	player.body_entered.connect(on_body_entered)
+#	hide()
+#	var player = get_node(".")
+	# player.body_entered.connect(on_body_entered)
 	
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
 	
-func on_body_entered(body):
-	hide()
-	hit.emit()
-	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred("disabled",true)
+#func on_body_entered(body):
+#	hide()
+#	hit.emit()
+#	# Must be deferred as we can't change physics properties on a physics callback.
+#	$CollisionShape2D.set_deferred("disabled",true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
